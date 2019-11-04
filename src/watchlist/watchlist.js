70f2612fc4a6617b0830049065e55366ab7b29f6
Merkel <merkel.julian@web.de>
2019-11-04 17:52:27 +0100

    localstorage klappt

--- src/watchlist/watchlist.js
+++ src/watchlist/watchlist.js
@@ -7,27 +7,84 @@ class Watchlist {
 
     constructor(app){
         this.app = app;
+
     }
 
-    onShow() {
+     onShow() {
         //create Container from imported HTML
+
         let container = document.createElement("div");
         container.innerHTML = watchlist.trim();
 
         // Anzuzeigende HTML-Elemente ermitteln
         let section = container.querySelector("#watchlist").cloneNode(true);
-        let content = {
+        this._watchlist = section.querySelector("#watchlist > main > div");
+
+
+        //damits beim ersten start nicht kracht
+        if(JSON.parse(localStorage.getItem("items2"))==null){
+            var arrayWatchlist=[];
+            localStorage.setItem("items2",JSON.stringify(arrayWatchlist));
+
+
+        }else{
+            var arrayWatchlist =JSON.parse(localStorage.getItem("items2"));
+            console.log(arrayWatchlist);
+            this.buildData(arrayWatchlist);
+        }
+
+
+         //Button
+         let button =  document.createElement("BUTTON");
+         button.innerHTML="Button";
+         this._watchlist.appendChild(button);
+
+         button.addEventListener('click', () => {
+
+                 arrayWatchlist.push("Lappen");
+                 localStorage.setItem("items2",JSON.stringify(arrayWatchlist));
+                 let data2 =JSON.parse(localStorage.getItem("items2"));
+
+                 console.log(data2);
+
+                 this.buildData(data2);
+
+
+         });
+
+
+
+
+         let content = {
             className: "watchlist",
             main: section.querySelectorAll("main > *"),
+
         };
+
+       //  this.createHeader();
 
         // Ergebnis zurückliefern
         return content;
     }
 
     get title() {
-        return "Übersicht";
+        return "Watchlist";
     }
+
+    buildData(data) {
+
+            this._watchlist.innerHTML = this._watchlist.innerHTML+`
+            <div>
+                <h1>`+data+ `</h1><br/>
+            </div>
+            `;
+
+
+    }
+
+
+
+
 }
 
 export default Watchlist;
